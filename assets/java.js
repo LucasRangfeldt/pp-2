e8b2717827759d392142b77c5084451a52d28ad9
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2023-07-27 21:11:03 +0200

    found a way to make the entire quiz in javascript, going with this instead. Deleted Quiz questions and answers from HTML

--- assets/java.js
+++ assets/java.js
@@ -1,146 +1,124 @@
 /*
     The following code is based on assistance provided by OpenAI and my buddy Mike, thanks Mike!
 */
+  // quiz-questions
+  
+  const myQuestions = [
+      {
+        question: "Question 1: In what year did Adolf Hitler become Chancellor for Germany?",
+        answers: {
+            a: "1928",
+            b: "1931",
+            c: "1933"
+        },
+        correctAnswer: "c"
+      },
+     {
+        question: "Question 2: Who was a part of the allies during World War 2?",
+        answers: {
+            a: 'Italy, China, United States and Great Britain',
+            b: 'France, United States, Great Britain, China and the Soviet Union',
+            c: 'Finland, France, United States and Great Britain'
+            
+        },
+        correctAnswer: 'b'
+     },
+     {
+        question: "Question 3: What Was The Codename For The Allied Invasion Of Normandy June 6th 1944?",
+        answers: {
+            a: 'Operation D-day',
+            b: 'Operation Barbarossa',
+            c: 'Operation Overlord'
+        },
+        correctAnswer: 'c'
+     },
+     {
+        question: "Question 4: Who was a part of the Axis?",
+        answers: {
+            a: 'Italy, Japan and Germany',
+            b: 'China, Japan and Germany',
+            c: 'Germany, Soviet Union and Japan'
+        },
+        correctAnswer: 'a'
+     },
+     {
+        question: "Question 5: Who was the prime minister of Great Britain most of the time during the war?",
+        answers: {
+            a: 'Winston Churchill' ,
+            b: 'Clemont Attlee' ,
+            c: 'Neville Chamberlain'
+        },
+        correctAnswer: 'a'
+     },
+     {
+        question: "Question 6: What was it that made the US enter the war and what year?",
+        answers: {
+            a: 'Imminent threat of nuclear bombs from the Axis, 1940',
+            b: 'They had lent so much money to the allies that if they did not win, they would likely not get their money back, 1942',
+            c: 'Attack on Pearl Harbor, 1941'
+        },
+        correctAnswer: 'c'
+     },
+     {
+        question: "Question 7: How many people lost their lives during the war?",
+        answers: {
+            a: '100 million',
+            b: '30 million',
+            c: '70 million'
+        },
+        correctAnswer: 'c'
+     },
+     {
+        question: "Question 8: What was the turning point for the Eastern front, marking a major defeat for the german army?",
+        answers: {
+            a: 'Battle of Verdun',
+            b: 'Battle of Stalingrad',
+            c: 'Normandy Landings'
+        },
+        correctAnswer: 'b'
+     },
+     {
+        question: "Question 9: What was the event that started the war in 1939?",
+        answers: {
+            a: 'Germany invades Poland',
+            b: 'France refuses to comply with the Nazi regime, causing Germany to declare war on France',
+            c: 'Great Britain declares war on Germany'
+        },
+        correctAnswer: 'a'
+     },
+     {
+        question: "Question 10: Which country was invaded and occupied by Germany during the war, leading to the establishment of the Vichy government?",
+        answers: {
+            a: 'Italy',
+            b: 'France',
+            c: 'Finland'
+        },
+        correctAnswer: 'b'
+     },
+    ];
 
-// Different functions required for the quiz.
-function quiz(questions, quizContainer, resultsContainer, checkAnswerButton, endScreen) {
-    function showQuestions(question, quizContainer) {
-        var output = [];
-        for (var i = 0; i < question.length; i++) {
-        answers = [];
-        for (letter in question[i].answers) {
-          answers.push(
-            '<label>' +
-            '<input type="radio" name="question' +
-            i +
-            '" value="' +
-            letter +
-            '">' +
-            letter +
-            ': ' +
-            question[i].answers[letter] +
-            '</label>'
-          );
+    const questionElement = document.getElementById('question');
+    const answersElement = document.getElementById('answers');
+    const prevButton = document.getElementById('previous');
+    const nextButton = document.getElementById('next');
+
+    let currentQuestionIndex = 0;
+
+    function showQuestion(index) {
+        const question = questions[0];
+        questionElement.textContent = question.question;
+
+        answersElement.innerHTML = '';
+        for (const option in question.answers) {
+            const label = document.createElement('label');
+            const input = document.createElement('input');
+            input.type = 'radio';
+            input.name = 'answer';
+            input.value = 'option';
+            label.appendChild(input);
+            label.appendChild(document.createTextNode(`${option.toUppercase()}: ${question.answers[option]}`));
+            answersElement.appendChild(label);
         }
-  
-        output.push(
-          '<div class="question">' + question[i].question + '</div>' + '<div class="answers">' + answers.join('') + '</div>'
-        );
-      }
-  
-      quizContainer.innerHTML = output.join('');
+        
     }
-  
-    function showResults(questions, quizContainer, resultsContainer) {
-      var answerContainers = quizContainer.querySelectorAll('.answers');
-      var userAnswer = '';
-      var numCorrect = 0;
-  
-      for (var i = 0; i < questions.length; i++) {
-        userAnswer = (
-          quizContainer.querySelector('input[name=question' + i + ']:checked') || {}
-        ).value;
-  
-        if (userAnswer === questions[i].correctAnswer) {
-          numCorrect++;
-          answerContainers[i].style.color = 'green';
-        } else {
-          answerContainers[i].style.color = 'red';
-        }
-      }
-  
-      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-  
-    showQuestions(questions, quizContainer);
-  
-    checkAnswerButton.onclick = function () {
-      showResults(questions, quizContainer, resultsContainer);
-    };
-  }
-
-        
-            answers: {
-                a: 'Battle of Verdun',
-                b: 'Battle of Stalingrad',
-                c: 'Normandy Landings'
-            },
-        
-            correctAnswer: 'b'
-        },
-        
-        {
-            question: 'What was the event that started the war in 1939?',
-        
-            answers: {
-                a: 'Britain declares war on Germany',
-                b: 'Germany invades Poland',
-                c: 'France refuses to comply with the Nazi regime, causing a chain reaction'
-            },
-        
-            correctAnswer: 'b'
-        },
-        
-        {
-            question: 'Which country was invaded and occupied by Germany during the war, leading to the establishment of the Vichy government?',
-        
-            answers: {
-                a: 'France',
-                b: 'Norway',
-                c: 'Denmark'
-            },
-        
-            correctAnswer: 'a'
-        },
-        
-        ];
-  
-  var quizContainer = document.getElementById('quiz');
-  var resultsContainer = document.getElementById('score');
-  var checkAnswerButton = document.getElementById('check-Answer');
-  quiz(myQuestions, quizContainer, resultsContainer, checkAnswerButton);
-
-  var currentQuestionIndex = 0;
-  
-  
-  var prevButton = document.getElementById('previous');
-  var nextButton = document.getElementById('next')
-  var checkAnswerButton = document.getElementById('check-Answer');
-
-  function showQuestion() {
-    if (currentQuestionIndex === myQuestions.length - 1) {
-        toggleCheckAnswerButton(true);
-    } else {
-        toggleCheckAnswerButton(false);
-    }
-  }
-
-  function toggleCheckAnswerButton(show) {
-    if (show) {
-        checkAnswerButton.style.display = 'block';
-    } else {
-        checkAnswerButton.style.display = 'none';
-    }
-  }
-
-
-  function nextQuestion() {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < myQuestions.length) {
-        showQuestion(currentQuestionIndex);
-    }   
     
-    prevButton.removeAttribute('disabled');
-  }
-
-  function previousQuestion() {
-    if (currentQuestionIndex > 0) {
-        currentQuestionIndex--;
-        showQuestion(currentQuestionIndex);
-    }
-    if (currentQuestionIndex === 0) {
-        prevButton.setAttribute('disabled', 'true');
-    }
-  }
-
-  
