354110ac6e603f2fb502e1f55b20d19cda1479c8
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2023-08-01 20:06:40 +0200

    add update section, add textarea for quiz suggestions, add inputs for email and name, fixed so the image regarding the quiz is somewhat responsive, more on that next

--- assets/java.js
+++ assets/java.js
@@ -9,9 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 1: In what year did Adolf Hitler become Chancellor for Germany?",
             image: '<img src="../pp-2/assets/images/1.jpg" alt="Nazi gathering during world war 2" id="question-1">',
             answers: {
-                a: "1928",
-                b: "1931",
-                c: "1933"
+                a: "1928.",
+                b: "1931.",
+                c: "1933."
             },
             correctAnswer: "c"
            
@@ -20,9 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 2: Who was a part of the allies during World War 2?",
             image: '<img src="../pp-2/assets/images/2.jpg" alt="Allied soldier holding his helmet with a big smile" id="question-2">',
             answers: {
-                a: 'Italy, China, United States and Great Britain',
-                b: 'France, United States, Great Britain, China and the Soviet Union',
-                c: 'Finland, France, United States and Great Britain'
+                a: 'Italy, China, United States and Great Britain.',
+                b: 'France, United States, Great Britain, China and Soviet Union.',
+                c: 'Finland, France, United States and Great Britain.'
                 
             },
             correctAnswer: 'b'
@@ -32,9 +32,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 3: What Was The Codename For The Allied Invasion Of Normandy June 6th 1944?",
             image: '<img src="../pp-2/assets/images/3.jpg" alt="Picture of Normandy, present day" id="question-3">',
             answers: {
-                a: 'Operation D-day',
-                b: 'Operation Barbarossa',
-                c: 'Operation Overlord'
+                a: 'Operation D-day.',
+                b: 'Operation Barbarossa.',
+                c: 'Operation Overlord.'
             },
             correctAnswer: 'c'
          },
@@ -42,9 +42,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 4: Who was a part of the Axis?",
             image: '<img src="../pp-2/assets/images/4.jpg" alt="Germans outside a cinema" id="question-4">',
             answers: {
-                a: 'Italy, Japan and Germany',
-                b: 'China, Japan and Germany',
-                c: 'Germany, Soviet Union and Japan'
+                a: 'Italy, Japan and Germany.',
+                b: 'China, Japan and Germany.',
+                c: 'Germany, Soviet Union and Japan.'
             },
             correctAnswer: 'a'
          },
@@ -52,9 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 5: Who was the prime minister of Great Britain most of the time during the war?",
             image: '<img src="../pp-2/assets/images/5.jpg" alt="Highly ranked british official, during the war" id="question-5">',
             answers: {
-                a: 'Winston Churchill' ,
-                b: 'Clemont Attlee' ,
-                c: 'Neville Chamberlain'
+                a: 'Winston Churchill.' ,
+                b: 'Clemont Attlee.' ,
+                c: 'Neville Chamberlain.'
             },
             correctAnswer: 'a'
          },
@@ -63,8 +63,8 @@ document.addEventListener('DOMContentLoaded', () => {
             image: '<img src="../pp-2/assets/images/6.jpg" alt="American warship" id="question-6">',
             answers: {
                 a: 'Imminent threat of nuclear bombs from the Axis, 1940',
-                b: 'They had lent so much money to the allies that if they did not win, they would likely not get their money back, 1942',
-                c: 'Attack on Pearl Harbor, 1941'
+                b: 'They had lent so much money to the allies that if they did not win, they would likely not get their money back, 1942.',
+                c: 'Attack on Pearl Harbor, 1941.'
             },
             correctAnswer: 'c'
          },
@@ -72,9 +72,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 7: How many people lost their lives during the war?",
             image: '<img src="../pp-2/assets/images/7.jpg" alt="Train entrance to Auschwitz" id="question-7">',
             answers: {
-                a: '100 million',
-                b: '30 million',
-                c: '70 million'
+                a: '100 million.',
+                b: '30 million.',
+                c: '70 million.'
             },
             correctAnswer: 'c'
          },
@@ -82,9 +82,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 8: What was the turning point for the Eastern front, marking a major defeat for the german army?",
             image: '<img src="../pp-2/assets/images/8.jpg" alt="Soldier raising the Soviet flag" id="question-8">',
             answers: {
-                a: 'Battle of Verdun',
-                b: 'Battle of Stalingrad',
-                c: 'Normandy Landings'
+                a: 'Battle of Verdun.',
+                b: 'Battle of Stalingrad.',
+                c: 'Normandy Landings.'
             },
             correctAnswer: 'b'
          },
@@ -92,9 +92,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 9: What was the event that started the war in 1939?",
             image: '<img src="../pp-2/assets/images/9.jpg" alt="Fighter plane terrifying the skies" id="question-9">',
             answers: {
-                a: 'Germany invades Poland',
-                b: 'France refuses to comply with the Nazi regime, causing Germany to declare war on France',
-                c: 'Great Britain declares war on Germany'
+                a: 'Germany invades Poland.',
+                b: 'France refuses to comply with the Nazi regime, causing Germany to declare war on France.',
+                c: 'Great Britain declares war on Germany.'
             },
             correctAnswer: 'a'
          },
@@ -102,9 +102,9 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "Question 10: Which country was invaded and occupied by Germany during the war, leading to the establishment of the Vichy government?",
             image: '<img src="../pp-2/assets/images/10.jpg" alt="Allied soldiers outside a long, broken building" id="question-10">',
             answers: {
-                a: 'Italy',
-                b: 'France',
-                c: 'Finland'
+                a: 'Italy.',
+                b: 'France.',
+                c: 'Finland.'
             },
             correctAnswer: 'b'
          },
@@ -147,6 +147,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 } 
                 label.appendChild(input);
                 label.appendChild(document.createTextNode(`${option.toUpperCase()}: ${question.answers[option]}`));
+                label.appendChild(document.createElement('br'));
                 answersElement.appendChild(label);
             }
             // Disables previous button if you are on the first question
